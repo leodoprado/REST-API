8fbdb58a0603aa9edc218234c6c1829348f4fb71
Leonardo do Prado <leonardodoprado231@gmail.com>
2026-06-11 09:39:27 -0300

    feat: 2 beyond the bascis

---
+++ 2.BeyondTheBasics/Movies.Application/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Movies.Application.Models;
+
+public class Movie
+{
+    public required Guid Id { get; init; }
+    
+    public required string Title { get; set; }
+
+    public string Slug => GeneratedSlug();
+    
+    public required int YearOfRelease { get; set; }
+
+    public required List<string> Genres { get; init; } = new();
+
+    private string GeneratedSlug()
+    {
+        var sluggedTitle = Regex.Replace(Title, "[^0-9A-Za-z _-]", string.Empty)
+            .ToLower().Replace(" ", "-");
+        
+        return $"{sluggedTitle}-{YearOfRelease}";
+    }
+}
