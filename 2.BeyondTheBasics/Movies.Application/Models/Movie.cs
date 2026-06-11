e0a663d7d2dbc814000b05617502bda3d8047fb1
Leonardo do Prado <leonardodoprado231@gmail.com>
2026-06-11 14:09:09 -0300

    feat: att 2

--- 2.BeyondTheBasics/Movies.Application/Models/Movie.cs
+++ 2.BeyondTheBasics/Movies.Application/Models/Movie.cs
@@ -2,7 +2,7 @@
 
 namespace Movies.Application.Models;
 
-public class Movie
+public partial class Movie
 {
     public required Guid Id { get; init; }
     
@@ -16,9 +16,12 @@ public class Movie
 
     private string GeneratedSlug()
     {
-        var sluggedTitle = Regex.Replace(Title, "[^0-9A-Za-z _-]", string.Empty)
+        var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
             .ToLower().Replace(" ", "-");
         
         return $"{sluggedTitle}-{YearOfRelease}";
     }
+
+    [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+    private static partial Regex SlugRegex();
 }
