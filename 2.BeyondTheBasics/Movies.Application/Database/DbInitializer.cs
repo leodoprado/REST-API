2f3f3645a442a60f42f6fbec096935cdd7cae6b9
Leonardo do Prado <leonardodoprado231@gmail.com>
2026-06-12 13:56:32 -0300

    feat: att

--- 2.BeyondTheBasics/Movies.Application/Database/DbInitializer.cs
+++ 2.BeyondTheBasics/Movies.Application/Database/DbInitializer.cs
@@ -1,6 +1,34 @@
-﻿namespace Movies.Application.Database;
+﻿using Dapper;
+
+namespace Movies.Application.Database;
 
 public class DbInitializer
 {
-    
-}
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+
+        await connection.ExecuteAsync("""
+                                        CREATE TABLE IF NOT EXISTS movies (
+                                            id UUID PRIMARY KEY,
+                                            slug TEXT NOT NULL,
+                                            title TEXT NOT NULL,
+                                            yearofrelease INTEGER NOT NULL
+                                        );
+                                      """);
+
+        await connection.ExecuteAsync("""
+                                      CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS movies_slug_idx
+                                      ON movies
+                                      using btree(slug);
+                                      """);
+                                      
+    }
+}   
